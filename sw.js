a02fc29206455c0de5137c2d362483b235a570fe
astridogo75-spec <astridogo75@gmail.com>
2026-06-21 16:09:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
-const CACHE = "programme12-v1";
+const CACHE = "programme12-v2";
 const FILES = ["./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => cached))
+    fetch(e.request).then((res) => {
+      caches.open(CACHE).then((c) => c.put(e.request, res.clone())).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
