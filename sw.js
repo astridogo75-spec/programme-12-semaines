7f8ae1fe523783498ca120ac917004135a81ac4e
astridogo75-spec <astridogo75@gmail.com>
2026-06-27 14:46:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "programme12-v3";
+const CACHE = "programme12-v4";
 const FILES = ["./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
